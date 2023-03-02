46e4648e85e9070b4b46749adca8502b7be1f167
sergiohdljr <sergio.tjf1@gmail.com>
2023-03-02 16:29:13 -0300

    setup: config react-query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "react-query";
 import { App } from "./App";
+import { Client } from "./service/service";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-      <App/>
+    <QueryClientProvider client={Client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
