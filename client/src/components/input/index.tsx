bfd3cda75e0e7d14a5d66f69147ac88a6dc3e977
sergiohdljr <sergio.tjf1@gmail.com>
2023-03-02 21:11:53 -0300

    feat: post task on api

--- client/src/components/input/index.tsx
+++ client/src/components/input/index.tsx
@@ -3,26 +3,30 @@ import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { SubmitHandler } from "react-hook-form/dist/types";
+import { api } from "../../service/api";
 
 export const InputNewTask = () => {
-  
   const schema = z.object({
     task: z
       .string()
       .min(1, { message: "Digite a tarefa que deseja adicionar" }),
   });
 
-  type validationField = z.infer<typeof schema> 
+  type validationField = z.infer<typeof schema>;
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<validationField>({resolver:zodResolver(schema)});
+  } = useForm<validationField>({ resolver: zodResolver(schema) });
 
   const OnSubmit: SubmitHandler<validationField> = (data) => {
-    console.log(data.task)
-  }
+    api
+      .post("/createTask", {
+        task: data.task,
+      })
+      .then((response) => console.log(response.status));
+  };
 
   return (
     <div>
@@ -35,7 +39,7 @@ export const InputNewTask = () => {
           {...register("task")}
         />
       </Input>
-      {errors ? <ErrorMessage>{errors.task?.message}</ErrorMessage> : null }
+      {errors ? <ErrorMessage>{errors.task?.message}</ErrorMessage> : null}
     </div>
   );
 };
